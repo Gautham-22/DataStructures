8714cbb3059762d1c619c8f8c818d4a730254612
Gautham-22 <gautham22poy@gmail.com>
2021-12-12 21:39:45 +0530

    added prims & dijkstra algorithm

---
+++ graph/dijkstra.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <limits.h>
+
+#define MAX 999
+#define V 6
+
+int minkey(int key[], bool mstset[]) {
+    int min = INT_MAX, min_index;
+
+    for(int v=0; v < V; v++) {
+        if(mstset[v] == false && key[v] < min) {
+            min = key[v];
+            min_index = v;
+        }
+    }
+    return min_index;
+}
+
+void shortest_path(int key[]) {
+    printf("Shortest path from vertex 0 to: \n");
+    printf("Vertex \t Weight\n");
+    for(int i=1; i < V; i++) {
+        printf("%d \t %d \n",i,key[i]);
+    }
+}
+
+void dijkstra(int graph[V][V]) {
+    int key[V];
+    bool mstset[V];
+
+    for(int i=0; i<V; i++) {
+        key[i]= INT_MAX;
+        mstset[i] = false;
+    }
+    key[0] = 0;
+
+    for(int count = 0; count < V-1; count++) {
+        int u = minkey(key, mstset);
+        mstset[u] = true;
+
+        for(int v=0; v < V; v++) {
+            if(graph[u][v] && mstset[v] == false && graph[u][v] + key[u] < key[v]) {
+                key[v] = graph[u][v] + key[u];
+            }
+        }
+    }
+    shortest_path(key);
+}
+
+int main() {
+    printf("Number of vertices: ");
+    int v;
+    scanf("%d", &v);
+
+    int graph[v][v];
+    for(int i=0; i<v; ++i) {
+        for(int j=0; j<v; ++j) {
+            if(i == j) {
+                graph[i][j] = 0;	
+            } else {
+                graph[i][j] = MAX;
+            }
+        }
+    }
+
+    printf("Enter the number of edges: ");
+    int e;
+    scanf("%d", &e);
+    for(int i=0; i<e; ++i) {
+        int v1, v2, cost;
+        printf("Enter the vertices v1, v2 and their cost: ");
+        scanf("%d %d %d", &v1, &v2, &cost);
+        graph[v1][v2] = cost;
+        graph[v2][v1] = cost;
+    }
+
+    dijkstra(graph);
+    return 0;
+}
